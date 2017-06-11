2bdef8760f690d30104d2dac2d6588247bf9771b
Eliakin del Rosario <eking1566@gmail.com>
2017-06-11 11:17:43 -0400

    Added a self build list function as well as a proper execution loop

--- quicksort/main.py
+++ quicksort/main.py
@@ -1,9 +1,30 @@
 from quicksort import *
 from util import *
+from time import time
+from random import randint
+from sys import exit
 
-num = getInput()
-print("Your orignal list was:\n",num)
-quicksort_(num)
-print("Your sorted list is:\n",num)
 
+
+# num = getInput()
+
+def setup():
+	x = [] # empty list
+	# add 100 random elements to the 
+	# empty list
+	for i in range(0, 100):
+	    x.append(randint(0, 100))
+	# return full list
+	return x
+
+def main():
+	x = setup() # list with 100 elements
+	start = time() # program start time
+	print("Before: {}".format(x))
+	quicksort_(x)
+	print("After: {}".format(x))
+	print("{0:.5f} seconds".format(time() - start)) # program end time
+
+if __name__ == "__main__":
+	exit(main())
 
